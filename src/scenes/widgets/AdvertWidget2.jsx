77fd66ad9975f44d460d1d7ba7fe0feaa4666132
Yossi <asnafy.junior@gmail.com>
2023-04-07 16:17:50 +0300

    https://friendly-server.onrender.com

--- src/scenes/widgets/AdvertWidget2.jsx
+++ src/scenes/widgets/AdvertWidget2.jsx
@@ -20,7 +20,7 @@ const AdvertWidget = () => {
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/download.png"
+        src="https://friendly-server.onrender.com/assets/download.png"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
