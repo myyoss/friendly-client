ed649ee4457d2f0c1dec8d4c4d90f7c420f1b23d
Yossi <asnafy.junior@gmail.com>
2023-04-10 13:39:13 +0300

    Update index.jsx

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -38,7 +38,7 @@ const Navbar = () => {
   const background = theme.palette.background.default;
   const alt = theme.palette.background.alt;
 
-  const fullName = `user ? ${user.firstName} ${user.lastName}`;
+  const fullName = `user ? ${user.firstName} ${user.lastName}: "jaze"`;
 
   return (
     <FlexBetween padding="1rem 6%" backgroundColor={alt}>
